bc186babc186fc9d97763c90a30e96f659da7136
balde17 <abdramanebalde4@gmail.com>
2025-04-24 19:56:42 +0000

    add: navbar, products page

--- src/app/components/add-produit/add-produit.component.ts
+++ src/app/components/add-produit/add-produit.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-produit',
-  imports: [],
+  imports: [ReactiveFormsModule],
+  standalone: true,
   templateUrl: './add-produit.component.html',
   styleUrl: './add-produit.component.css'
 })
 export class AddProduitComponent {
+  produitForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.produitForm = this.fb.group({
+      nom: ['', Validators.required],
+      description: ['', Validators.required],
+      prix: [null, [Validators.required, Validators.min(0)]],
+      quantite: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.produitForm.valid) {
+      console.log("Produit ajouté", this.produitForm.value);
+
+      this.produitForm.reset();
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
