da4bd6b7836a182f1365ed6ad59ec68abda8bac9
balde17 <abdramanebalde4@gmail.com>
2025-04-30 19:03:40 +0000

    add: add_product form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProduitsComponent } from './components/produits/produits.component';
 import { AddProduitComponent } from './components/add-produit/add-produit.component';
+
 export const routes: Routes = [
     {path: "produits", component : ProduitsComponent},
     {path: "add-produit", component: AddProduitComponent},
